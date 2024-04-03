c7f7a55623396103cfafb4a0eb50ee2f2fe61545
Goblin Oats <hello@tonk.gg>
2024-04-03 17:49:35 +0100

    fix: dangling import commented out

--- playground/game/packages/client/src/gribi/Example.ts
+++ playground/game/packages/client/src/gribi/Example.ts
@@ -1,23 +1,18 @@
-import { PrivateEntry } from '@gribi/vault';
 import { NetworkCall } from '@gribi/mud';
 import { keccak256, toHex, encodePacked } from 'viem';
-// import ClaimLoot from '../../../circuits/claimLoot/target/claimLoot.json';
-// import RevealLoot from '../../../circuits/revealLoot/target/revealLoot.json';
 
-const MODULE_ID = BigInt(keccak256(toHex("example-module")));
+// import { }
+
+
+
 
 export type ModuleCalls = ReturnType<typeof createModuleCalls>;
 
-type ExampleValue = {
-    salt: string,
-    secret: string 
-}
-
-interface ExampleCommitmentEntry extends PrivateEntry {
-    value: ExampleValue
-}
-
 export function createModuleCalls(call: NetworkCall) {
+
+    const createCommitment = (secret: number) => {
+
+    }
 
     return {
         // createCommitment,
